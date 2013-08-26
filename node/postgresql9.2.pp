6f34a24edc5e1254e1060667f0e8999a6951d1a4
Joachim Fermstad <joachim.fermstad@kentor.se>
2013-08-26 11:27:30 +0200

    Added steps for postgresql initdb

--- node/postgresql9.2.pp
+++ node/postgresql9.2.pp
@@ -7,7 +7,8 @@ augeas { "exclude postgresql":
   ],
 }
 
-package { 'postgresql92':
-  ensure => installed,
-}
+package { 'postgresql92': ensure => installed, }
+package { 'postgresql92-server': ensure => installed, }
 
+exec { "/sbin/service postgresql-9.2 initdb": creates => "/var/lib/pgsql/9.2/data/PG_VERSION" }
+service { "postgresql-9.2": ensure => "running", }
