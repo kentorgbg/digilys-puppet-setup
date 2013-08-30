247c6648b8ca467a2de12b731da2096d907338a5
Andreas Johansson <andreas@stdin.se>
2013-08-30 10:49:39 +0200

    Added nginx to the digilys module

--- modules/digilys/manifests/init.pp
+++ modules/digilys/manifests/init.pp
@@ -11,4 +11,10 @@ class digilys {
   $digilys_instances = hiera("digilys_instances", [])
 
   create_resources(digilys::instance, $digilys_instances)
+
+  # nginx, uses $digilys_instances in the template
+  include nginx
+  nginx::file { "digilys.conf":
+    content => template("digilys/nginx.conf.erb")
+  }
 }
