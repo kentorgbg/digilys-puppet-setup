d6ef2e22faaa2febf699db8722d920a386969bce
Andreas Johansson <andreas@stdin.se>
2013-08-30 11:20:24 +0200

    Added package dependencies.

--- modules/digilys/manifests/init.pp
+++ modules/digilys/manifests/init.pp
@@ -1,11 +1,14 @@
 class digilys {
-
-  # Dependencies
-
   # Global setup, postgres
   # Settings are picked up using hiera
   include postgresql
   include postgresql::server
+
+  # Dependencies
+  if ! defined(Package["libxml2-devel"])      { package { "libxml2-devel":      ensure => installed } }
+  if ! defined(Package["libxslt-devel"])      { package { "libxslt-devel":      ensure => installed } }
+  if ! defined(Package["postgresql92-devel"]) { package { "postgresql92-devel": ensure => installed } }
+  if ! defined(Package["libcurl-devel"])      { package { "libcurl-devel":      ensure => installed } }
 
   # Instances
   $digilys_instances = hiera("digilys_instances", [])
