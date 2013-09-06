aa7f32c9e0531e2ba84df527cc45d01fe3aa7bdd
Andreas Johansson <andreas@stdin.se>
2013-09-06 14:15:58 +0200

    Allow external access for readonly users

--- modules/digilys/manifests/readonly_user.pp
+++ modules/digilys/manifests/readonly_user.pp
@@ -1,6 +1,7 @@
 define digilys::readonly_user(
   $username = $title,
-  $password
+  $password,
+  $external = false
 ) {
   postgresql::database_user { $username:
     password_hash => postgresql_password($username, $password)
@@ -8,5 +9,16 @@ define digilys::readonly_user(
   postgresql_psql { "GRANT digilys_readonly TO ${username}":
     db     => $postgresql::params::user,
     unless => "SELECT 1 FROM pg_user LEFT JOIN pg_auth_members ON member = usesysid LEFT JOIN pg_roles ON roleid = pg_roles.oid WHERE usename = '${username}' AND rolname = 'digilys_readonly'"
+  }
+
+  if ($external) {
+    postgresql::pg_hba_rule { "Allow readonly access externally to ${username}":
+      description => "up readonly access externally for ${user}",
+      type        => "host",
+      database    => "all",
+      user        => $username,
+      auth_method => "md5",
+      address     => $external
+    }
   }
 }
